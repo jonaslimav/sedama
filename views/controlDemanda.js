ba59f5184e66c74cf1c12e72c75758cac30f8c97
jonas lima <jlvieira248@gmail.com>
2022-01-12 10:55:24 -0300

    Atualizaçãoa

---
+++ views/controlDemanda.js
@@ -0,0 +1,244 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCalbx4hqCxrVt500RKGIb-ENwIHs9stAU",
+    authDomain: "sedama-4dd5c.firebaseapp.com",
+    databaseURL: "https://sedama-4dd5c-default-rtdb.firebaseio.com",
+    projectId: "sedama-4dd5c",
+    storageBucket: "sedama-4dd5c.appspot.com",
+    messagingSenderId: "492670080550",
+    appId: "1:492670080550:web:42ad24f36c7f5cd578b8e7"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+window.onload = listar;
+
+
+
+var valor= 71.74;
+
+function InserirProtocolo() {
+
+
+
+   
+   var databaseRef = firebase.database().ref('demanda22/');
+   
+   
+
+        let demanda_id = false;
+
+
+        const demanda = {
+    
+            
+            nomeProdutor: nomeProdutor = document.getElementById("produtor").value.toUpperCase(),
+            cpf: cpf = document.getElementById("cpf").value,
+            localidade: localidade = document.getElementById ("localidade").value.toUpperCase(),
+            rg: rg = document.getElementById("rg").value,
+            quant:quant = Number(document.getElementById("quant").value),
+            item: item = document.getElementById("item").value,
+            date:new Date()*-1,
+            dap:dap =document.getElementById("dap").value,
+            
+            
+        };
+    
+        if (!demanda_id) {
+            demanda_id = firebase.database().ref().child('demanda22').push().key;
+        }
+        let updates = {}
+        updates["/demanda22/" + demanda_id] = demanda;
+        let demanda_ref = firebase.database().ref();
+        firebase.database().ref().update(updates);
+        window.location.reload();
+    }
+    
+   
+  
+
+function listar() {
+	
+	
+    if(!localStorage.getItem("auth")){
+        alert("Necessario fazer login");
+      window.location.href = "loguin.html";
+
+    }
+
+    var tblUsers = document.getElementById('tbl_users_list');
+    var databaseRef = firebase.database().ref('demanda22/');
+    var rowIndex = 1;
+    var horasTr=0;
+    var dias=0;
+    var dataAnt;
+    databaseRef.orderByChild("date").once('value', function (snapshot) {
+        
+        snapshot.forEach(function (childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+
+            var row = tblUsers.insertRow(rowIndex);
+            var cellNome = row.insertCell(0);
+            var cellCPF = row.insertCell(1);
+            var cellLocalidade = row.insertCell(2);
+            var cellRG = row.insertCell(3);            
+            var cellItem= row.insertCell(4);
+            var cellQuant = row.insertCell(5);
+            var cellTel=row.insertCell(6);
+            var cellImprimir = row.insertCell(7);
+            
+          
+            
+            cellNome.appendChild(document.createTextNode(childData.nomeProdutor));
+            cellCPF.appendChild(document.createTextNode(childData.cpf));
+            cellLocalidade.appendChild(document.createTextNode(childData.localidade));
+            cellRG.appendChild(document.createTextNode(childData.rg));
+            cellItem.appendChild(document.createTextNode(childData.item));
+            cellQuant.appendChild(document.createTextNode(childData.quant));
+            cellTel.appendChild(document.createTextNode(childData.dap));
+            cellImprimir.innerHTML='<input type="button" class="btn btn-danger" value="IMPR." onclick="imprimir(this)"}/>';
+
+          
+        });
+
+    });
+    
+}
+
+
+
+
+
+function imprimir(dt){
+
+var data=dt.parentNode.parentNode.children;
+var nomePr = data[0].innerHTML;
+var cpfPr = data[1].innerHTML;
+var localPr = data[2].innerHTML;
+var rgPr = data[3].innerHTML;
+var itemPr = data[4].innerHTML;
+var quantPr = data[5].innerHTML;
+var dap =data[6].innerHTML;
+
+var x = document.getElementById("geral");
+
+x.innerHTML = `
+            ________________________________________________________________________________________________________________________________
+				<h3> Via Produtor</h3> <img src="../demandahp.png" height=220 width=95%><h1> 
+				<h1>
+                <br> <strong> PRODUTOR:</strong>&nbsp ${nomePr} &nbsp &nbsp &nbsp &nbsp<strong>CPF:</strong> &nbsp  ${cpfPr}<br><br>
+                <strong>DAP:</strong>&nbsp   ${dap} <br><br>
+                <strong>RG Nº:</strong>&nbsp   ${rgPr} &nbsp &nbsp &nbsp &nbsp
+                <strong> Item:</strong>  &nbsp ${itemPr} &nbsp&nbsp&nbsp&nbsp&nbsp <strong> QUANT:  </strong>  ${quantPr} &nbsp&nbsp <br> <br>
+                <strong>LOCALIDADE:</strong>  &nbsp ${localPr} &nbsp&nbsp&nbsp&nbsp <strong> <br><br>
+				<strong> SERVIDOR:______________________________________ <br><br>  DATA ENTREGA:______/______/__________</strong><br>
+				<br>	<img src="../logotrator.png" alt="some text" height=200 width=90%>
+
+                    <h1>_____________________________________________________________________________________________________________________<br>
+		
+
+					_______________________________________________________________________________________________________________________<br>
+					<h3> Via SEDAMA</h3><img src="../demandahp.png" height=220 width=95%><h1>
+                    <br> <strong> PRODUTOR:</strong>&nbsp ${nomePr} &nbsp &nbsp &nbsp &nbsp<strong>CPF:</strong> &nbsp  ${cpfPr}<br><br>
+                    <strong>DAP:</strong>&nbsp   ${dap} <br><br>
+                    <strong>RG:</strong>&nbsp   ${rgPr} &nbsp &nbsp &nbsp &nbsp
+                    <strong> ITEM:</strong>  &nbsp ${itemPr}<br><br>
+                    <strong>LOCALIDADE:</strong>  &nbsp ${localPr} &nbsp &nbsp &nbsp &nbsp 
+                    <strong> QUANT.:  </strong>  ${quantPr}<br><br>
+					 <strong>DATA ENTREGA:______/______/__________</strong><br><br>
+					<strong> ASS. PRODUTOR: _____________________________________</strong><br><br>
+					<img src="../logotrator.png" alt="some text" height=200 width=90% >
+                        </h1>`; 
+                    
+                    
+  //  printDiv();
+
+}
+
+function printDiv() {
+   var conteudo = document.getElementById("geral").innerHTML;
+    var win = window.open();
+    win.document.write(conteudo);
+    win.print();
+    win.close();//Fecha após a impressão.  
+	
+}
+
+function horasFormat(horas){
+
+  var hora= String(horas).substring(0,1);
+    var minutos= ((Number(horas)-Number(hora))*60).toFixed(0);
+    if(minutos==0){
+        minutos="";
+    }else{
+        minutos=minutos+" Min";
+    }
+    return hora+' Hr '+ minutos;
+}
+function sair(){
+
+    localStorage.clear();
+    window.location.href="PROTRATOR.html";
+}
+function completaDados(){
+   
+    var rowIndex = 0;
+    if(rowIndex==0){
+
+        var databaseRef = firebase.database().ref('trator2021/');
+       
+       
+        var cpf = document.getElementById("cpf").value
+    
+        databaseRef.orderByChild("date").once('value', function (snapshot) {
+            
+            snapshot.forEach(function (childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+               
+                if((childData.cpf == cpf)&& rowIndex==0){
+    
+                    document.getElementById("produtor").value =childData.nomeProdutor;
+                    document.getElementById ("localidade").value =childData.localidade;
+                     document.getElementById("rg").value= childData.rg;
+                     document.getElementById("tel").value = childData.telefone;
+                   rowIndex ++;
+              
+                }
+             
+                
+               
+              
+              
+            });
+    
+        });
+    } 
+    if(rowIndex==0){
+       
+        var databaseRef = firebase.database().ref('anuencia/');
+        databaseRef.orderByChild("date").once('value', function (snapshot) {
+        
+            snapshot.forEach(function (childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                console.log(`${cpf} == ${childData.cpf}`);
+                if((childData.cpf == cpf)&& rowIndex==0){
+    
+                    document.getElementById("produtor").value =childData.nomeProdutor;
+                    document.getElementById ("localidade").value =childData.localidade;
+                   rowIndex ++;
+              
+                }
+
+            });
+    
+        });
+    }
+    
+
+
+
+    
+
+}
