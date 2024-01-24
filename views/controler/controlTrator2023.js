a0d09cd2085df7cc24a17a72023f5e7579c74dff
jonaslimav <62893835+jonaslimav@users.noreply.github.com>
2024-01-24 11:17:36 -0300

    Update controlTrator2023.js

--- views/controler/controlTrator2023.js
+++ views/controler/controlTrator2023.js
@@ -95,7 +95,7 @@ function listar() {
         console.log(localStorage.getItem("user"));
  if(localStorage.getItem("user")=="wandeilsonviana@hotmail.com"||localStorage.getItem("user")=="kalitianne1@gmail.com" ){
 
-         window.location.href = "views/PROTRATOR2024.html";
+         window.location.href = "PROTRATOR2024.html";
       
 
     }
