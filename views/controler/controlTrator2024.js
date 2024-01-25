1cf9edd2e790a20d30a506b65172545e1c1be0b6
jonaslimav <62893835+jonaslimav@users.noreply.github.com>
2024-01-25 11:00:27 -0300

    Update controlTrator2024.js

--- views/controler/controlTrator2024.js
+++ views/controler/controlTrator2024.js
@@ -40,7 +40,7 @@ var horasTotais = 0;
        horasTotais = horasTotais+Number(document.getElementById("horas").value);
        console.log(horasT);
        
-       var horasLimite= 1200;
+       var horasLimite= 500;
        if(horasTotais>horasLimite && localStorage.getItem("user")=="wandeilsonviana@hotmail.com"){
         alert(` Horas ultrapassam o Limite do periodo (${horasLimite}hrs): ${horasFormat(horasTotais)}!!! `);
 
