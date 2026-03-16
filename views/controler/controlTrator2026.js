6b7ead0c7312b894eb45741887a55aaeb7450971
jonaslimav <62893835+jonaslimav@users.noreply.github.com>
2026-03-16 07:38:17 -0300

    Atualizar o controlTrator2026.js

--- views/controler/controlTrator2026.js
+++ views/controler/controlTrator2026.js
@@ -41,7 +41,7 @@ var horasTotais = 0;
        horasTotais = horasTotais+Number(document.getElementById("horas").value);
        console.log(horasT);
        
-       var horasLimite= 1468;
+       var horasLimite= 5000;
        if(cpf.length<11){
         alert(`FORMATO CPF INCORRETO!!! `);
 
